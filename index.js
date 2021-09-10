c314ece43e2b17384a2a489c03c592a6d50475b9
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2021-09-10 16:59:19 +0900

    backup

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-// 1回innerHtmlに変換してからやる方法
-// const parseHtmlString = (html) => {
-//     const arr = html.split(/\<|\>/).filter(str => str.length > 0);
-//     const stack = [];
-  
-//     const lessSign = "&lt;"
-//     const greaterSign = "&gt;\n"
-//     const space = "\t"
-  
-//     for(let i=0; i<arr.length; i++){
-//         if(arr[i].charAt(0) != "/"){
-//             stack.push([i, arr[i]])
-//             continue;
-//         }
-//         const arr1 = stack.pop();
-//         arr[arr1[0]] = space.repeat(stack.length) + lessSign + arr1[1] + greaterSign;
-//         arr[i] = space.repeat(stack.length) + lessSign + arr[i] + greaterSign;
-//     }
-  
-//     return arr.join("");
-// }
-
-// document.getElementById("translate").addEventListener("click", function(){
-//     const input = document.getElementById("code-input").value;
-//     const code  = document.getElementById("code");
-//     code.innerHTML = parseHtmlString(input);
-// })
-
 const node0 = new Node(0, "div", [], "top", 0, []);
 const node1 = new Node(1, "div", [], "", 1, []);
 const node2 = new Node(2, "div", [], "", 2, []);
