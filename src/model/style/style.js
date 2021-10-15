320f5a67222e7442fba91490645d22f939bdeb57
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2021-10-15 19:45:28 +0900

    modelないにstyleフォルダを配置

---
+++ src/model/style/style.js
@@ -0,0 +1,27 @@
+// class Style{
+//     static space = "\t"
+    
+//     constructor(property, value, unit){
+//         this.property = property;
+//         this.value    = value;
+//         this.unit     = unit;
+//     }
+//     createStyleBody(){
+//         return `${Style.space}${this.getProperty()}: ${this.getValue()}${this.getUnit()};`
+//     }
+//     getProperty(){
+//         return this.property == null ? "" : this.property;
+//     }
+//     getValue(){
+//         return this.value == null ? "" : this.value;
+//     }
+//     getUnit(){
+//         return this.unit == null ? "" :this.unit;
+//     }
+// }
+
+class Style{
+    createStyleBody(){
+        throw "must be implemented";
+    }
+}
