d58eb702fecb3eb8a6a4da44485ff25cd7835008
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2021-10-16 17:13:17 +0900

    grid_rowクラスを作成

--- src/model/style/grid_row.js
+++ src/model/style/grid_row.js
@@ -1,4 +1,4 @@
-class GridColumn extends Style{
+class GridRow extends Style{
     createStyleBody(){
         return `${this.constructor.space}${this.getProperty()}: ${this.getValue().get("start")}/${this.getValue().get("end")}`;
     }
